36b1efbb0b16a53b17a4709fdd27462866b9a0cc
Sergey Arshavin <arshavin3111772@hotmail.com>
2016-09-07 10:50:57 +0200

    initial envrionment

---
+++ my-new-project/app/scripts/app.js
@@ -0,0 +1,32 @@
+'use strict';
+
+/**
+ * @ngdoc overview
+ * @name testAppApp
+ * @description
+ * # testAppApp
+ *
+ * Main module of the application.
+ */
+angular
+  .module('testAppApp', [
+    'ngAnimate',
+    'ngCookies',
+    'ngResource',
+    'ngRoute',
+    'ngSanitize',
+    'ngTouch',
+    'ui.calendar',
+    'angular.less'
+  ])
+  .config(function ($routeProvider) {
+    $routeProvider
+      .when('/', {
+        templateUrl: 'views/main.html',
+        controller: 'MainCtrl',
+        controllerAs: 'main'
+      })
+      .otherwise({
+        redirectTo: '/'
+      });
+  });
---
+++ my-new-project/app/scripts/controllers/main.js
@@ -0,0 +1,33 @@
+'use strict';
+
+/**
+ * @ngdoc function
+ * @name testAppApp.controller:MainCtrl
+ * @description
+ * # MainCtrl
+ * Controller of the testAppApp
+ */
+angular.module('testAppApp')
+  .controller('MainCtrl', function () {
+    this.awesomeThings = [
+      'HTML5 Boilerplate',
+      'AngularJS',
+      'Karma'
+    ];
+
+    $scope.uiConfig = {
+      calendar:{
+        height: 450,
+        editable: true,
+        header:{
+          left: 'month basicWeek basicDay agendaWeek agendaDay',
+          center: 'title',
+          right: 'today prev,next'
+        },
+        eventClick: $scope.alertEventOnClick,
+        eventDrop: $scope.alertOnDrop,
+        eventResize: $scope.alertOnResize
+      }
+    };
+
+  });
